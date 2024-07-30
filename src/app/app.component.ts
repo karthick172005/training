7197b2f12381febb707939f6d7e0f0550a7ed00d
training <karthickkrishna2005@gmail.com>
2024-07-30 14:33:29 +0530

    title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'SATHYABAMA';
+  title = 'everyone';
   desc = 'welcome to sathyabama pride';
 }
