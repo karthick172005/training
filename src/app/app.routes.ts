d1b568a8df0c6b814566bf62c893a1d56412ab86
training <karthickkrishna2005@gmail.com>
2024-07-31 11:59:42 +0530

    created pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'signup',
+        pathMatch: 'full'
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'signup',
+        component:SignupComponent
+    },
+    {
+        path:'dashboard',
+        component:DashboardComponent
+    }
+];
